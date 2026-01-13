10352eb25cf9b7dc4bf8c3a13050c745dc1ba221
FinnMacNeil <macneilfinn@gmail.com>
2026-01-13 15:49:07 +0000

    Update PTPv2.py
    begin logic for monitoring clock sync between devices

--- test environment/PTPv2.py
+++ test environment/PTPv2.py
@@ -1,3 +1,21 @@
-# logic for clock synchronization using PTPv2 would go here
+# logic for monitoring clock synchronization using PTPv2 would go here
+import socket
+import struct
 
+class PTPMonitor:
+
+    def __init__(self): 
+        self.ptp_detected = False
+        self.master_clock_id = None
+
+    def check_ptp_traffic(self, timeout=5): # Check for PTPv2 traffic on the network to detect master clock
+        PTP_EVENT_PORT = 319#
+
+        try: 
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind(("", PTP_EVENT_PORT))
+
+            # Join multicast group for PTPv2
+            MCAST_GRP = "
 # failsafes for clock asynchrony would go here 
